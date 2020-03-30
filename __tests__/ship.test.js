e0ed5e8f0f20a6557e6b50b224b3e075a429b548
Justin Sandahl <sandahl.j.d@gmail.com>
2020-03-30 21:41:07 +0100

    setup first tests for Ship

---
+++ __tests__/ship.test.js
@@ -0,0 +1,13 @@
+const Ship = require("../src/ship");
+
+describe ('Ship', () => {
+    it('creates and object instances', () => {
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
+
+    it('creates and object instances', () => {
+        const ship = new Ship ('Liverpool');
+
+        expect(ship.startingPort).toBe('Liverpool');
+    });
+});
