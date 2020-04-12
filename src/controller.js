5c90b25f3a8cff45ba1422fa2ef85a08373a4159
Justin Sandahl <sandahl.j.d@gmail.com>
2020-04-12 15:53:05 +0100

    created a renderPorts method within the Controller class, this allows the controller to create new Port elements onto the page based on the intinerary created within the inline js scripted added to the index.html file.

--- src/controller.js
+++ src/controller.js
@@ -3,18 +3,38 @@
     this.initialiseSea();
   }
 
-  Controller.prototype.initialiseSea = function initialiseSea() {
-    const backgrounds = ["./images/water0.png", "./images/water1.png"];
+  Controller.prototype = {
+    initialiseSea() {
+      const backgrounds = ["./images/water0.png", "./images/water1.png"];
 
-    let backgroundIndex = 0;
+      let backgroundIndex = 0;
 
-    window.setInterval(() => {
-      //console.log("test-intervals");
-      document.querySelector("#viewport").style.backgroundImage = `url('${
-        backgrounds[backgroundIndex % backgrounds.length]
-      }')`;
-      backgroundIndex += 1;
-    }, 1000);
+      window.setInterval(() => {
+        //console.log("test-intervals");
+        document.querySelector("#viewport").style.backgroundImage = `url('${
+          backgrounds[backgroundIndex % backgrounds.length]
+        }')`;
+        backgroundIndex += 1;
+      }, 1000);
+    },
+
+    renderPorts(ports) {
+      const portsElement = document.querySelector("#ports");
+      portsElement.style.width = "0px";
+
+      ports.forEach((port, index) => {
+        const newPortElement = document.createElement("div");
+        newPortElement.dataset.portName = port.name;
+        newPortElement.dataset.portIndex = index;
+        newPortElement.className = "port";
+
+        portsElement.appendChild(newPortElement);
+
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`;
+
+      });
+    },
   };
 
   if (typeof module !== "undefined" && module.exports) {
